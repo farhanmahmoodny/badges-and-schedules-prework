eb6ff2b21bcb39a7858e3ba7fa0a62708f5b003d
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 07:13:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,6 @@ return room_assignments
 end
 
 def printer(attendees)
-puts batch_badge_creator(attendees[0])
-puts assign_rooms(attendees[0])
+puts batch_badge_creator(attendees).
+puts assign_rooms(attendees)
 end
