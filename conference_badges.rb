6197e8367990df52361338e3e963fb396b64b41c
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 06:45:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   counter = 0
-  until counter == attendees.length-1
+  until counter == attendees.length
   badges.push(badge_maker(attendees[counter]))
   counter += 1
 end
